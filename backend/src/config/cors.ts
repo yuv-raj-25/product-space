9e8e4eef86cd40618a3a4dc11532b4110d59cfe1
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2026-04-28 23:50:04 +0530

    depl: added the client url

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,8 +1,8 @@
 import { env } from "./env";
 
 const defaultAllowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
+  // "http://localhost:5173",
+  // "http://127.0.0.1:5173",
   "https://product-space-tawny.vercel.app"
 ];
 
